286d039bca04279ece3f75460575847918b3d186
pnurmi <pnurmi.zt@gmail.com>
2025-07-16 15:45:07 +0300

    fixed perror messages

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: pnurmi <pnurmi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 10:43:38 by pnurmi            #+#    #+#             */
-/*   Updated: 2025/07/16 13:35:18 by pnurmi           ###   ########.fr       */
+/*   Updated: 2025/07/16 15:43:53 by pnurmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ void	kid_one(char *argv[], char *envp[], int *pipefd)
 	char	**cmd_args;
 
 	cmd_args = cmd_parse(argv[2]);
-	cmd_path = cmd_check(&cmd_args[0], *envp);
+	cmd_path = cmd_check(envp, cmd_args[0]);
 	if (!cmd_path)
 	{
-		ft_putstr_fd("Command not found", 2);
 		ft_putstr_fd(cmd_args[0], 2);
+		ft_putstr_fd(": command not found", 2);
 		ft_putstr_fd("\n", 2);
 		cleanup_and_exit(cmd_args, NULL, 127);
 	}
@@ -36,6 +36,7 @@ void	kid_one(char *argv[], char *envp[], int *pipefd)
 	close(pipefd[0]);
 	close(pipefd[1]);
 	execve(cmd_path, cmd_args, envp);
+	perror("execve");
 	cleanup_and_exit(cmd_args, cmd_path, 1);
 }
 void	kid_two(char *argv[], char *envp[], int *pipefd)
@@ -45,11 +46,11 @@ void	kid_two(char *argv[], char *envp[], int *pipefd)
 	char	**cmd_args;
 
 	cmd_args = cmd_parse(argv[3]);
-	cmd_path = cmd_check(&cmd_args[0], *envp);
+	cmd_path = cmd_check(envp, cmd_args[0]);
 	if (!cmd_path)
 	{
-		ft_putstr_fd("Command not found", 2);
 		ft_putstr_fd(cmd_args[0], 2);
+		ft_putstr_fd(": command not found", 2);
 		ft_putstr_fd("\n", 2);
 		cleanup_and_exit(cmd_args, NULL, 127);
 	}
@@ -62,6 +63,7 @@ void	kid_two(char *argv[], char *envp[], int *pipefd)
 	close(pipefd[0]);
 	close(pipefd[1]);
 	execve(cmd_path, cmd_args, envp);
+	perror("execve");
 	cleanup_and_exit(cmd_args, cmd_path, 1);
 }
 
